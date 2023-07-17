373616e1835b7ab2567bcd157e59172c5084055f
Varun19980901 <139058075+Varun19980901@users.noreply.github.com>
2023-07-17 10:06:48 +0530

    Add files via upload

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,34 @@
+
+makeMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+cacheinverse <- function(x, ...) {
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+}
+
+
+x <- matrix(1:4,2,2)
+
+amatrix <- makeMatrix(x)
+amatrix$get()
+amatrix$getinverse()
+cacheinverse(amatrix)
